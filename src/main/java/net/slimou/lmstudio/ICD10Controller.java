05622f452ffa3869911813f7842cf371db11c186
SalimOussayfi <box@slimou.de>
2025-02-13 06:53:49 +0100

    ICD Code search from UI

--- src/main/java/net/slimou/lmstudio/ICD10Controller.java
+++ src/main/java/net/slimou/lmstudio/ICD10Controller.java
@@ -1,4 +1,4 @@
-package net.slimou.lmstudio.controller;
+package net.slimou.lmstudio;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
